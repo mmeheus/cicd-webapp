5d957241cb7a5de4d2cbdd8627ae84e405a23ef8
mmeheus <mathieu.meheus@student.hogent.be>
2024-10-12 13:27:15 +0200

    eslint errors fixed

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import pkg from "@eslint/js"; // Import the entire package as a default export
+import pkg from '@eslint/js'; // Import the entire package as a default export
 const { eslintRecommended } = pkg; // Extract the eslintRecommended property
 
 export default [
   {
     ...eslintRecommended, // Spread the recommended configuration
-    files: ["**/*.js"], // Targets all JS files
+    files: ['**/*.js'], // Targets all JS files
     languageOptions: {
       ecmaVersion: 2021, // Use ECMAScript 2021
-      sourceType: "module", // Enable ES module syntax
+      sourceType: 'module', // Enable ES module syntax
       globals: {
         es6: true, // Enable ES6 environment
         node: true, // Enable Node.js environment
@@ -16,16 +16,16 @@ export default [
     },
     rules: {
       // Migrate your custom rules
-      indent: ["error", 2, { SwitchCase: 1 }],
-      "linebreak-style": ["error", "unix"],
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
-      "comma-dangle": ["error", "always-multiline"],
-      "no-tabs": ["error"],
-      "max-len": ["error", { code: 120, tabWidth: 2 }],
-      "arrow-parens": ["error", "always"],
-      "brace-style": ["error", "1tbs", { allowSingleLine: false }],
-      "no-inner-declarations": "off",
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-tabs': ['error'],
+      'max-len': ['error', { code: 120, tabWidth: 2 }],
+      'arrow-parens': ['error', 'always'],
+      'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+      'no-inner-declarations': 'off',
     },
   },
 ];
